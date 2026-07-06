50d6f325a55d03ffa129fb89b390a0a40c851ef7
armstrong-khisa <armstrong.khisa@student.moringaschool.com>
2026-07-06 01:57:51 -0700

    add functionality to connect to SQLite database and list tables

--- connecting-to-db.py
+++ connecting-to-db.py
@@ -1,2 +1,16 @@
 import sqlite3
 import pandas as pd
+
+conn = sqlite3.connect("data.sqlite")
+
+cur = conn.cursor()
+
+cur.execute("""SELECT name FROM sqlite_master WHERE type='table';""")
+
+tables = cur.fetchall()
+
+print("Tables in the database:")
+for table in tables:
+    print(table[0])
+
+conn.close()
